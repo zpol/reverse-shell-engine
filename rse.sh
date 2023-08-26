d7f121aa3551b5fbbaae114b270463710feff5ec
Pol <poljane007@gmail.com>
2023-08-26 23:11:52 +0200

    adding some IP & port checking

--- rse.sh
+++ rse.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 
 GR='\033[0;32m'
+RE='\033[0;31m'
+OR='\033[0;33m'
 NC='\033[0m'
+
 
 function b() {
 	bb="ICAgLnRAQEBAQEBYQEBAQEBAQFhAQEBAQEBAWEBAQEBAQEB0IC4gIAogLlguUzt0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dEA6UyAgCiBTOCB0QDhTJVggO0AlOC46Ojo6Ojo6Ojo6Ojo6Ojo6OjouLiA4JSAKIFhALjg4OEAgLiUlJVN0IC4gLi4uLi4uLi4uLi4uLi4uLiAgIEBAIAogQFguLlN0O3RAOzt0WC4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uQEAgCiBAOCBYQFNYWEBYWFhAQEBAWEBYQFhAWEBYQFhAWEBYQFhYWC44QCAKIEA4JSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC4gJThAIAogQDglIC4gLjo4QEB0LiAgLiAuICAuICAuICAuICAuICAuICAlOFggCiBAOCUgICAgIEA6Ljh0Oy4gICAgICAuICAgICAgLiAgICAgLiU4QCAKIEA4JSAgLiAgLjo4OiAgdCAgLiAuICAgIC4gLiAgIC4gICAgJThYLgogQDglICAgIC4gIC47WC5AOjogICAgIC4gICAgICAgICAuICAlOEAgCiBAOCUgIC4gICAgIC47QCA4WCAgIC4gICAuICAuIC4gICAuICU4WCAKIEA4JSAgICAgLiAuJTouOzggIC4gICAgICAgICAgICAuICAgJThAIAogQDglIC4gIC4gOCA6QDhTIDo4QEBAQFhAQFhAUyAuICAgIC4lOEAgCiBAOCUgICAgOjt0ODhTICAgUzo4QEBAQEBAQEAgICAgIC4gICU4WCAKIEA4dCAgLiAgLjogICAgIC4gLiAgLiAgICAuICAgLiAgICAgdDhAIAogOzs6ODouLjouLjouOi46Li46LjouLjouOi4uOi4uOi46OjgudFMgCiAuU3QgQDhAOEA4QDhAOEA4QDhAOEA4QDhAOEA4QDhAOEA4QDhACgo="
@@ -13,6 +16,8 @@ function b() {
 function rebuild_index() {
 	find tpl -type f -iname '*.tpl' | cut -d"/" -f2 | sed "s|.tpl$||" | sort > tpl/index
 }
+
+
 
 function get_tpl() {
 	TPL_FILE=$1
@@ -45,14 +50,49 @@ function generate() {
 	get_tpl ${TPL_NAME} ${CC_SERVER_IP} ${PORT}
 }
 
+function check_ip() {
+	local ip="$1"
+    local ip_pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+
+    if [[ $ip =~ $ip_pattern ]]; then
+        IFS='.' read -ra ip_parts <<< "$ip"
+
+        if [ ${#ip_parts[@]} -eq 4 ]; then
+            for part in "${ip_parts[@]}"; do
+                if ! [[ "$part" =~ ^[0-9]+$ ]] || ((part < 0 || part > 255)); then
+					echo -e "${RE}>> [ERR]: Invalid IP addr${NC}"
+                    return 1  
+					exit 2
+                fi
+            done
+            return 0  
+        fi
+    fi
+
+	echo -e "${RE}>> [ERR]: Invalid IP addr${NC}"
+	exit 1
+	
+}
 
 
+function check_port() {
+
+ 	local ip_port="$1"
+
+	if [ ${ip_port} == "0" -o ${ip_port} -gt 65535 ]; then 
+		echo -e "${RE}>> ERR: Incorrect port${NC}"
+		exit 3
+	fi
+
+}
 
 function selector() {
 	echo -n -e "${GR}>> Type IPv4 address where to listen on: ${NC}"
 	read ip_addr
+	check_ip ${ip_addr}
 	echo -n -e "${GR}>> Now the TCP port where to listen on: ${NC}" 
 	read tcp_port
+	check_port ${tcp_port}
 	TPL=$(cat tpl/index | percol --prompt ">>> Select a reverse shell type to generate")
 	generate ${TPL} ${ip_addr} ${tcp_port}
 	
